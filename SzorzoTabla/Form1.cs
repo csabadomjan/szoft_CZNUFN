bc35243ce3d121fcf3c4224862843ac34c352ccd
csabadomjan <csaba.domjan@stud.uni-corvinus.hu>
2024-02-22 12:56:23 +0100

    Add project files.

---
+++ SzorzoTabla/Form1.cs
@@ -0,0 +1,31 @@
+namespace SzorzoTabla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int sor = 0; sor < 10; sor++)
+            {
+                for (int oszlop = 0; oszlop < 10; oszlop++)
+                {
+
+
+                    Button b = new Button();
+                    b.Width = 50;
+                    b.Height = 50;
+                    b.Left = oszlop * 40;
+                    b.Top = sor * 40;
+                    b.Text = ((sor+1) * (oszlop+1)).ToString();
+                    Controls.Add(b);
+                }
+            }
+            
+            
+        }
+    }
+}

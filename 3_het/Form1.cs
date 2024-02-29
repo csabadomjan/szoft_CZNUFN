c1dc56135da070054a4ba1a269a157b9c0f70080
csabadomjan <csaba.domjan@stud.uni-corvinus.hu>
2024-02-29 16:54:42 +0100

    3.gyakorlat

---
+++ 3_het/Form1.cs
@@ -0,0 +1,38 @@
+namespace _3_het
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button b = new Button();
+            Controls.Add(b);
+
+            b.Left = ClientRectangle.Width / 2 - b.Width / 2;
+            b.Top = ClientRectangle.Height / 2 - b.Height / 2;
+
+
+            int méret = 40;
+
+            for (int sor = 0; sor < 20; sor++)
+            {
+
+                for (int oszlop = 0; oszlop < 20; oszlop++)
+                {
+
+                    Szamologomb p = new Szamologomb();
+                    Controls.Add(p);
+                    p.Height = méret;
+                    p.Width = méret;
+                    p.Left = méret * oszlop;
+                    p.Top = méret * sor;
+                    p.BackColor = Color.FromArgb(64, 224, 208);
+                }
+            }
+        }
+    }
+}
---
+++ 3_het/Szamologomb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_het
+{
+    internal class Szamologomb : Villogogomb
+    {
+        int szam = 0;
+        public Szamologomb()
+        {
+            MouseClick += Szamologomb_MouseClick;
+        }
+
+        private void Szamologomb_MouseClick(object? sender, MouseEventArgs e)
+        {
+            if (szam <= 10)
+            {
+                szam++;
+            }
+            else
+            {
+                /*szam += 1;*/
+                szam = 0;
+
+
+
+            }
+            Text = szam.ToString();
+        }
+    }
+}
